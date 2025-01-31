b7c17b2c98ea7c73a63872c992ed6793caa4fb8c
jonat_elrom <57427529+jonathan0249160552@users.noreply.github.com>
2025-01-31 14:19:07 +0000

    Update index.php
    index touches

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['customer'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
 	<meta charset="UTF-8">
